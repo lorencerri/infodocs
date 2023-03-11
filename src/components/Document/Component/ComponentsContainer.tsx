b981aa8d89c26f1ae44af30a8d39cd2c0819c8d3
Loren Cerri <lorencerri@gmail.com>
2023-03-11 17:18:09 -0500

    Allow dragging of components

--- src/components/Document/Component/ComponentsContainer.tsx
+++ src/components/Document/Component/ComponentsContainer.tsx
@@ -1,40 +1,100 @@
-import { useEffect } from "react";
-import { ComponentType } from "@prisma/client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { useEffect, useState } from "react";
+import { Component, ComponentType } from "@prisma/client";
 import { api } from "~/utils/api";
 import { selectedDocumentAtom } from "~/atoms";
 import { TextComponent } from "./TextComponent";
 import { useAtom } from "jotai";
 import { refetchesAtom } from "~/atoms";
+import {
+  useSensors,
+  useSensor,
+  PointerSensor,
+  KeyboardSensor,
+  DndContext,
+  closestCenter,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
 
 export const CompontentsContainer = () => {
   const [selectedDocument] = useAtom(selectedDocumentAtom);
   const id = selectedDocument?.id || 0; // TODO: I don't think this is the proper way to do this.
   const [, setRefetches] = useAtom(refetchesAtom);
 
+  const [items, setItems] = useState<Component[]>([]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
   const { data, refetch } = api.component.getAllForDocument.useQuery({
     documentId: id,
   });
 
+  const handleDragEnd = (event: { active: any; over: any }) => {
+    const { active, over } = event;
+    console.log(active.id, over.id);
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+        console.log(oldIndex, newIndex);
+        const newArray = arrayMove(items, oldIndex, newIndex);
+        console.log(newArray);
+        return newArray;
+      });
+    }
+  };
+
   useEffect(() => {
-    if (!refetch) return;
-    console.log(refetch);
-    setRefetches((refetches) => ({ ...refetches, components: refetch }));
-  }, [setRefetches, refetch]);
+    if (refetch) {
+      setRefetches((refetches) => ({ ...refetches, components: refetch }));
+    } // Update refetches atom
+
+    if (data) {
+      setItems(data || []);
+    }
+  }, [setRefetches, refetch, data]); // TODO: Transform the data into the items array since it's a hook.
 
   return (
     <>
-      {data?.map((item) => {
-        switch (item.type) {
-          case ComponentType.TEXT:
-            return (
-              <TextComponent header={item.header} content={item.content} />
-            );
-          case ComponentType.VIDEO:
-            return <div>Video</div>;
-          default:
-            return <div>Unknown</div>;
-        }
-      })}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+        modifiers={[restrictToVerticalAxis]}
+      >
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          {items?.map((item) => {
+            switch (item.type) {
+              case ComponentType.TEXT:
+                return (
+                  <TextComponent
+                    key={item.id}
+                    id={item.id}
+                    header={item.header}
+                    content={item.id}
+                  />
+                );
+              case ComponentType.VIDEO:
+                return <div>Video</div>;
+              default:
+                return <div>Unknown</div>;
+            }
+          })}{" "}
+        </SortableContext>
+      </DndContext>
     </>
   );
 };
--- src/components/Document/Component/TextComponent.tsx
+++ src/components/Document/Component/TextComponent.tsx
@@ -1,12 +1,32 @@
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 export const TextComponent = ({
   header,
   content,
+  id,
 }: {
   header: string | null;
   content: string | null;
+  id: string;
 }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <div className="min-h-16 mb-5 w-full rounded-lg bg-base-200 p-5 text-center">
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="min-h-16 mb-5 w-full rounded-lg bg-base-200 p-5 text-center"
+    >
       <h1 className="text-2xl">{header}</h1>
       <p>{content}</p>
     </div>
