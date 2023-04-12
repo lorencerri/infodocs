983f902b775264e935c413ff97fb28beecdedd92
Loren Cerri <lorencerri@gmail.com>
2023-04-12 18:46:35 -0400

    Update DocumentsDisplay.tsx

--- src/components/Home/DocumentsDisplay.tsx
+++ src/components/Home/DocumentsDisplay.tsx
@@ -1,10 +1,14 @@
+import { api } from "~/utils/api";
+
 export const DocumentsDisplay = () => {
+  const { data, refetch } = api.document.getAll.useQuery();
+
   return (
     <div className="hero mt-3 bg-base-200 p-5 lg:mt-5 xl:rounded-lg">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-3xl font-bold lg:text-5xl">Documents</h1>
-          <p className="pt-6">TODO</p>
+          <p className="pt-6">{data?.map((doc) => doc.title)}</p>
         </div>
       </div>
     </div>
