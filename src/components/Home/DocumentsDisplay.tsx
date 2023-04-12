a5e3f6032b55c5b36d737d8138122dc66308c30c
Loren Cerri <lorencerri@gmail.com>
2023-04-12 18:44:15 -0400

    Update DocumentsDisplay.tsx

--- src/components/Home/DocumentsDisplay.tsx
+++ src/components/Home/DocumentsDisplay.tsx
@@ -3,11 +3,8 @@ export const DocumentsDisplay = () => {
     <div className="hero mt-3 bg-base-200 p-5 lg:mt-5 xl:rounded-lg">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-3xl font-bold lg:text-5xl">Knowledgebase</h1>
-          <p className="pt-6">
-            Welcome to our knowledgebase! Here, you can find information on our
-            various projects.
-          </p>
+          <h1 className="text-3xl font-bold lg:text-5xl">Documents</h1>
+          <p className="pt-6">TODO</p>
         </div>
       </div>
     </div>
